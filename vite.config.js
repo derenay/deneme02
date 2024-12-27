6a798d4b54005181bb32530d898e9c243e05d9c3
derenay <erenayarsal@hotmail.com>
2024-12-27 23:56:30 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/About/', // Örnek: /About/
+  base: '/deneme02/',  // Should match your repository name
 })
